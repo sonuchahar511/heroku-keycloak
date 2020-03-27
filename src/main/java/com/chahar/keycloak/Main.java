b1e8e5467641541fe088b9bba7c1fed5e58803a0
Sonu Singh Chahar <sonuchahar511@gmail.com>
2020-03-27 08:43:02 +0530

    commit first time heroku keycloak poc

---
+++ src/main/java/com/chahar/keycloak/Main.java
@@ -0,0 +1,74 @@
+package com.chahar.keycloak;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.config.undertow.BufferCache;
+import org.wildfly.swarm.config.undertow.HandlerConfiguration;
+import org.wildfly.swarm.config.undertow.Server;
+import org.wildfly.swarm.config.undertow.ServletContainer;
+import org.wildfly.swarm.config.undertow.server.HTTPListener;
+import org.wildfly.swarm.config.undertow.server.Host;
+import org.wildfly.swarm.config.undertow.servlet_container.JSPSetting;
+import org.wildfly.swarm.config.undertow.servlet_container.WebsocketsSetting;
+import org.wildfly.swarm.datasources.DatasourcesFraction;
+import org.wildfly.swarm.keycloak.server.KeycloakServerFraction;
+import org.wildfly.swarm.spi.api.SocketBinding;
+import org.wildfly.swarm.undertow.UndertowFraction;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        // Heroku requires you bind to the specified PORT
+        String port = System.getenv("PORT");
+        if (port != null) {
+            System.setProperty("swarm.http.port", port);
+        }
+
+        Swarm container = new Swarm();
+
+        // Extract the postgres connection details from the Heroku environment variable
+        // (which is not a JDBC URL)
+        DatabaseUrl databaseUrl = DatabaseUrl.extract();
+
+        // Configure the KeycloakDS datasource to use postgres
+        DatasourcesFraction datasourcesFraction = new DatasourcesFraction();
+        datasourcesFraction
+                .dataSource("KeycloakDS", (ds) -> {
+                    ds.jndiName("java:jboss/datasources/KeycloakDS");
+                    ds.driverName("postgresql");
+                    ds.connectionUrl(databaseUrl.jdbcUrl());
+                    ds.userName(databaseUrl.username());
+                    ds.password(databaseUrl.password());
+                });
+//
+        container.fraction(datasourcesFraction);
+
+        // Set up container config to take advantage of HTTPS in heroku
+        container.socketBinding("standard-sockets", new SocketBinding("proxy-https").port(443));
+
+        UndertowFraction undertowFraction = new UndertowFraction();
+        undertowFraction
+                .server(new Server("default-server")
+                        .httpListener(new HTTPListener("default")
+                                .socketBinding("http")
+                                .redirectSocket("proxy-https")
+                                .proxyAddressForwarding(true))
+                        .host(new Host("default-host")))
+                .bufferCache(new BufferCache("default"))
+                .servletContainer(new ServletContainer("default")
+                        .websocketsSetting(new WebsocketsSetting())
+                        .jspSetting(new JSPSetting()))
+                .handlerConfiguration(new HandlerConfiguration());
+
+        container.fraction(undertowFraction);
+
+        // Finally, add KeycloakServer...
+        KeycloakServerFraction keycloakServerFraction = new KeycloakServerFraction();
+
+        container.fraction(keycloakServerFraction.applyDefaults());
+
+        // And start!
+        container.start();
+    }
+}
